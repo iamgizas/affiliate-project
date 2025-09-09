eaff01c0dbbd58cb1c69a3e04379e377b3a5abfb
Gabriel.S <gabrielfagundessilveira@gmail.com>
2025-09-09 16:49:14 -0300

    [UPDATE]: Authorization for token added in index.js and userRegister have more params added

--- front/src/services/api/index.js
+++ front/src/services/api/index.js
@@ -1,15 +1,17 @@
+
 const request = (url, method = "POST", body) => {
     return fetch(`${process.env.REACT_APP_API_URL}${url}`, {
         method,
         body: JSON.stringify(body),
         headers: {
-            'content-type': 'application/json'
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
         }
     });
 }
 
-export const userRegister = (role, nome, email, senha) => {
-    return request(`/register`, "POST", {role, nome, email, senha});
+export const userRegister = (role, nome, email, cpf, telefone, senha) => {
+    return request(`/register`, "POST", {role, nome, email, cpf, telefone, senha});
 }
 
 export const userLogin = (email, senha) => {
